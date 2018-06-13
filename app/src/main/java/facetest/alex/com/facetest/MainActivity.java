d4a19683528c3238ac3b0749e67090fccd14d5ce
alexandrute <teodoroviciwork@gmail.com>
2018-06-13 13:13:17 +0300

    Initial commit

---
+++ app/src/main/java/facetest/alex/com/facetest/MainActivity.java
@@ -0,0 +1,176 @@
+package facetest.alex.com.facetest;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.microsoft.projectoxford.face.contract.Face;
+import com.microsoft.projectoxford.face.contract.FaceAttribute;
+import com.microsoft.projectoxford.face.contract.FaceRectangle;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+public class MainActivity extends Activity {
+
+    private final int PICK_IMAGE = 1;
+    private ProgressDialog detectionProgressDialog;
+    private FaceServiceClient faceServiceClient = new FaceServiceRestClient("YOUR_SERVICE_HOST", "YOUR_SUBSCRIPTION_KEY");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //pick a photo button
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("");
+                //start an intent to pick an image
+                Intent gallIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                gallIntent.setType("image/*");
+                startActivityForResult(Intent.createChooser(gallIntent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+        //instantiate the progress dialog for Kairos api call
+        detectionProgressDialog = new ProgressDialog(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            //process the chosen image
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+                imageView.setImageBitmap(bitmap);
+
+                detectAndFrame(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Detect faces by uploading face images
+    // Frame faces after detection
+    private void detectAndFrame(final Bitmap imageBitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        AsyncTask<InputStream, String, Face[]> detectTask = new AsyncTask<InputStream, String, Face[]>() {
+            @Override
+            protected Face[] doInBackground(InputStream... params) {
+                try {
+                    publishProgress("Detecting...");
+
+                    FaceServiceClient.FaceAttributeType[] requiredFaceAttributes = new FaceServiceClient.FaceAttributeType[]{
+                            FaceServiceClient.FaceAttributeType.Age,
+                            FaceServiceClient.FaceAttributeType.Gender,
+                            FaceServiceClient.FaceAttributeType.Smile,
+                    };
+
+                    Face[] result = faceServiceClient.detect(params[0], true, false, requiredFaceAttributes);
+
+                    if (result == null) {
+                        publishProgress("Detection Finished. Nothing detected");
+                        return null;
+                    }
+
+                    publishProgress(String.format("Detection Finished. %d face(s) detected", result.length));
+
+                    return result;
+                } catch (Exception e) {
+                    publishProgress("Detection failed");
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPreExecute() {
+                detectionProgressDialog.show();
+            }
+
+            @Override
+            protected void onProgressUpdate(String... progress) {
+                detectionProgressDialog.setMessage(progress[0]);
+            }
+
+            @Override
+            protected void onPostExecute(Face[] result) {
+                detectionProgressDialog.dismiss();
+                if (result == null) return;
+
+                ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+                imageView.setImageBitmap(drawFaceRectanglesOnBitmap(imageBitmap, result));
+                imageBitmap.recycle();
+
+                // Face face : result)
+                for (int i = 0; i < result.length; i++) {
+                    Face face = result[i];
+                    UUID id = face.faceId;
+                    FaceAttribute attributes = face.faceAttributes;
+                    double age = attributes.age;
+                    String gender = attributes.gender;
+                    double smile = attributes.smile;
+
+
+                    Log.d("FACETEST", "FACE " + i + " UUID: " + id.toString() + " age: " + age + " gender : " + gender + " Smile: " + smile);
+                    TextView textView = (TextView) findViewById(R.id.textView);
+                    textView.setText(textView.getText() + "FACE " + i + " age: " + age + " gender : " + gender + " Smile: " + smile + " \n");
+                }
+            }
+        };
+
+        //FaceAPI runs the face detection process
+        detectTask.execute(inputStream);
+    }
+
+    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
+        Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.GREEN);
+        int stokeWidth = 10;
+        paint.setStrokeWidth(stokeWidth);
+        if (faces != null) {
+            for (Face face : faces) {
+                FaceRectangle faceRectangle = face.faceRectangle;
+                canvas.drawRect(
+                        faceRectangle.left,
+                        faceRectangle.top,
+                        faceRectangle.left + faceRectangle.width,
+                        faceRectangle.top + faceRectangle.height,
+                        paint);
+            }
+        }
+        return bitmap;
+    }
+
+}
